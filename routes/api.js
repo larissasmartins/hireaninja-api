66add82177f78cf537edc345bb87ae03b450749a
Larissa Martins <larissa.martins@inboundcycle.com>
2023-08-09 09:03:14 -0300

    Fix: error handling, send message to the user and update status

--- routes/api.js
+++ routes/api.js
@@ -3,28 +3,28 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the database
-router.get('/ninjas', function (request, response) {
+router.get('/ninjas', function (request, response, next) {
     response.send({
         type: 'GET',
     });
 });
 
 // add a new ninja to the database
-router.post('/ninjas', function (request, response) {
+router.post('/ninjas', function (request, response, next) {
     Ninja.create(request.body).then(function (ninja) {
         response.send(ninja); //send back to the client
-    });
+    }).catch(next); //middleware for error handling
 });
 
 // update a ninja in the database
-router.put('/ninjas/:id', function (request, response) {
+router.put('/ninjas/:id', function (request, response, next) {
     response.send({
         type: 'PUT',
     });
 });
 
 // delete a ninja from the database
-router.delete('/ninjas/:id', function (request, response) {
+router.delete('/ninjas/:id', function (request, response, next) {
     response.send({
         type: 'DELETE',
     });
