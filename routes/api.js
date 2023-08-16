1b6ea9644146009d36374dcba60d2077dfe8657f
Larissa Martins <larissa.martins@inboundcycle.com>
2023-08-16 09:17:57 -0300

    Feat: handling put requests

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,7 @@ const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the database
 router.get('/ninjas', function (request, response, next) {
-    response.send({
-        type: 'GET',
-    });
+    response.send({ type: 'GET' });
 });
 
 // add a new ninja to the database
@@ -18,16 +16,20 @@ router.post('/ninjas', function (request, response, next) {
 
 // update a ninja in the database
 router.put('/ninjas/:id', function (request, response, next) {
-    response.send({
-        type: 'PUT',
+    Ninja.findByIdAndUpdate({ _id: request.params.id }, req.body).then(function () {
+        Ninja.findOne({ _id: request.params.id }).then(function (ninja) {
+            response.send(ninja);
+        });
     });
 });
 
 // delete a ninja from the database
 router.delete('/ninjas/:id', function (request, response, next) {
-    response.send({
-        type: 'DELETE',
+    // console.log(request.params.id);
+    Ninja.findByIdAndRemove({ _id: request.params.id }).then(function (ninja) {
+        request.send(ninja);
     });
+    // response.send({ type: 'DELETE' });
 });
 
 module.exports = router;
